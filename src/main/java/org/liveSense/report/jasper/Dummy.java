27cbf9991feef9783ccbac72f521a354e1bc5a69
Robert Csakany <csakany.robert@gmail.com>
2012-11-08 23:23:32 +0100

    Dummy file added (m2e plugion does not work correctly if there is no source file)

---
+++ src/main/java/org/liveSense/report/jasper/Dummy.java
@@ -0,0 +1,8 @@
+package org.liveSense.report.jasper;
+
+// TODO: Find the bug in m2e maven-bundle plugin collaboration.
+// The problem is if there is no source code related to a bundle the
+// maven bundle plugin does not work correctly with m2e
+public class Dummy {
+	
+}
